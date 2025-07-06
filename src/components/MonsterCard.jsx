f36eb9053d32cc214735db4a1fdab63aced6234e
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-06 10:16:49 +0700

    Update MonsterCard.jsx

--- src/components/MonsterCard.jsx
+++ src/components/MonsterCard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import monster from '../assets/monster.png';
 
 const MonsterCard = ({ hp }) => (
-  <div className="text-center">
-    <img src={monster} alt="Monster" className="w-40 mx-auto animate-bounce" />
+  <div className="text-center w-1/2 sm:w-1/3">
+    <img src={monster} alt="Monster" className="w-[120px] sm:w-[160px] mx-auto animate-bounce" />
     <p className="mt-2 text-lg">Monster HP: {hp}</p>
   </div>
 );
